b3845ce023ca8fbfa76036d46a523e60e21bdb3a
KOSASIH <kosasihg88@gmail.com>
2024-05-21 10:15:22 +0700

    Create data_processing.py

---
+++ utils/data_processing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def preprocess_data(data):
+    # Preprocessing steps
+    # ...
+    return preprocessed_data
+
+def split_data(data, test_size):
+    # Splitting data into training and testing sets
+    # ...
+    return X_train, y_train, X_test, y_test
